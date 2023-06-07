a3216c453a680c0f769538b16b677a54f9fc065e
elevchyt <lehy64@gmail.com>
2023-06-08 02:00:05 +0300

    home page layout with some animations

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -2,59 +2,77 @@ import { motion } from "framer-motion";
 import "./styles.scss";
 
 type PropsType = {
-  type: "primary" | "secondary";
+  type: "primary" | "secondary" | "primaryBig";
   text: string;
   functionality?: () => void;
 };
 
 const Button = (props: PropsType) => {
-  return (
-    <div className="Button">
-      {props.type === "primary" ? (
-        <motion.button
-          className="Button__primary"
-          whileHover={{
-            color: "#fe3c3c",
-            backgroundColor: "#ffffff",
-            scale: 1.05,
-            boxShadow: "4px 4px 0 rgba(0, 0, 0, 0.2)",
-          }}
-          whileTap={{
-            color: "#fe3c3c",
-            backgroundColor: "#ebebeb",
-            borderColor: "#fe3c3c",
-            scale: 1.0,
-            boxShadow: "1px 1px 0 rgba(0, 0, 0, 0.2)",
-          }}
-          transition={{ duration: 0.2 }}
-          onClick={props.functionality}
-        >
-          {props.text}
-        </motion.button>
-      ) : (
-        <motion.button
-          className="Button__secondary"
-          whileHover={{
-            color: "#ffffff",
-            backgroundColor: "#fe3c3c",
-            scale: 1.05,
-            boxShadow: "4px 4px 0 rgba(0, 0, 0, 0.2)",
-          }}
-          whileTap={{
-            color: "#ffffff",
-            backgroundColor: "#fe3c3c",
-            borderColor: "#fe3c3c",
-            scale: 1.0,
-            boxShadow: "1px 1px 0 rgba(0, 0, 0, 0.2)",
-          }}
-          transition={{ duration: 0.2 }}
-          onClick={props.functionality}
-        >
-          {props.text}
-        </motion.button>
-      )}
-    </div>
-  );
+  const renderButton = () => {
+    switch (props.type) {
+      case "primary":
+        return (
+          <motion.button
+            className="Button__primary"
+            whileHover={{
+              scale: 1.05,
+              boxShadow: "4px 4px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            whileTap={{
+              scale: 1.0,
+              boxShadow: "1px 1px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            transition={{ duration: 0.2 }}
+            onClick={props.functionality}
+          >
+            {props.text}
+          </motion.button>
+        );
+      case "secondary":
+        return (
+          <motion.button
+            className="Button__secondary"
+            whileHover={{
+              color: "#ffffff",
+              backgroundColor: "#fe3c3c",
+              scale: 1.05,
+              boxShadow: "4px 4px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            whileTap={{
+              color: "#ffffff",
+              backgroundColor: "#fe3c3c",
+              borderColor: "#fe3c3c",
+              scale: 1.0,
+              boxShadow: "1px 1px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            transition={{ duration: 0.2 }}
+            onClick={props.functionality}
+          >
+            {props.text}
+          </motion.button>
+        );
+      case "primaryBig":
+        return (
+          <motion.button
+            className="Button__primaryBig"
+            whileHover={{
+              scale: 1.05,
+              boxShadow: "8px 8px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            whileTap={{
+              scale: 1.0,
+              boxShadow: "1px 1px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            transition={{ duration: 0.2 }}
+            onClick={props.functionality}
+          >
+            {props.text}
+          </motion.button>
+        );
+    }
+  };
+
+  return <div className="Button">{renderButton()}</div>;
 };
 
 export default Button;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,52 @@
 import Patterns from "components/graphics/Patterns/Patterns";
 import "./styles.scss";
+import { motion } from "framer-motion";
+import graphicsHomeUrl from "assets/imgs/graphics-home.png";
+import Button from "components/ui/Button/Button";
+import { useTranslation } from "react-i18next";
 
 const Home = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <div className="Home__headingContainer">
+        <motion.h1
+          className="Home__title"
+          initial={{ x: -400 }}
+          animate={{ x: 0 }}
+          transition={{
+            delay: 0.3,
+            type: "spring",
+            bounce: 0.3,
+            stiffness: 50,
+          }}
+        >
+          ACHORDIO
+        </motion.h1>
+        <motion.h2
+          className="Home__subtitle"
+          initial={{ y: 50 }}
+          animate={{ y: 0 }}
+          transition={{ delay: 2, type: "spring", bounce: 0.5, stiffness: 120 }}
+        >
+          {t("HOME.SUBTITLE")}
+        </motion.h2>
+      </div>
+      <motion.div
+        className="Home__buttonSong"
+        initial={{ opacity: 0, scale: 1.2 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ delay: 3, type: "spring", bounce: 0.5, stiffness: 120 }}
+      >
+        <Button type={"primaryBig"} text={t("HOME.FIND_A_SONG")} />
+      </motion.div>
+
       <Patterns />
+      <div
+        className="Home__graphics"
+        style={{ backgroundImage: `url(${graphicsHomeUrl})` }}
+      ></div>
     </div>
   );
 };
