faba837b6f3bf35f12f7f886ffc8e5161e651186
elevchyt <lehy64@gmail.com>
2023-06-08 21:05:53 +0300

    modal header

--- src/components/modals/Modal/Modal.tsx
+++ src/components/modals/Modal/Modal.tsx
@@ -13,7 +13,29 @@ const Modal = (props: PropsType) => {
     <>
       {createPortal(
         <div className="Modal">
-          <div className="Modal__window"></div>
+          <div className="Modal__window">
+            <div className="Modal__header">
+              <div className="Modal__title">Σύνδεση</div>
+              <motion.div
+                className="Modal__closeButton"
+                whileHover={{ rotate: "180deg" }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="46px"
+                >
+                  <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+                </svg>
+              </motion.div>
+            </div>
+
+            <div className="Modal__body">
+              Consectetur ex elit culpa voluptate tempor dolore officia mollit
+              ullamco commodo mollit incididunt.
+            </div>
+          </div>
+
           <div className="Modal__backdrop"></div>
         </div>,
         document.getElementById("modal-portal")!
