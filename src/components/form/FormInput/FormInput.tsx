5691201bc625d1374f1e1329701123f37d2f084a
elevchyt <lehy64@gmail.com>
2023-06-10 20:03:17 +0300

    form input base

--- src/components/form/FormInput/FormInput.tsx
+++ src/components/form/FormInput/FormInput.tsx
@@ -1,7 +1,22 @@
 import "./styles.scss";
+import { Formik, Form, Field, FieldProps, ErrorMessage } from "formik";
 
-const FormInput = () => {
-  return <div className="FormInput"></div>;
+interface FormFieldProps extends FieldProps {
+  type: string;
+}
+
+const FormInput: React.FC<FormFieldProps> = ({
+  field,
+  form,
+  type,
+  ...props
+}) => {
+  return (
+    <div className="FormInput">
+      <input {...field} {...props} type={type} />
+      {/* <ErrorMessage name={field.name} component="div" /> */}
+    </div>
+  );
 };
 
 export default FormInput;
---
+++ src/components/form/LoginForm/LoginForm.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+import { Formik, Form, Field, FieldProps, ErrorMessage } from "formik";
+
+interface FormFieldProps extends FieldProps {
+  type: string;
+}
+
+const LoginForm = () => {
+  return <div className="LoginForm"></div>;
+};
+
+export default LoginForm;
